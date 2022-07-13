80d200d90f6695633476d5ade9dd2c8f79a40ccc
Kavita Manocha <kavita28978@gmail.com>
2022-07-13 11:29:25 +0530

    Inserted "-" to a string by using length of the string and relocated the logic of matching edittext content with regex expression to afterTextChangedMethod.

--- app/src/main/java/com/components/MainActivity.kt
+++ app/src/main/java/com/components/MainActivity.kt
@@ -5,7 +5,6 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.components.databinding.ActivityMainBinding
@@ -15,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var card_no: String? = null
     private val cardPattern = "^[A-Z]{2}[ -][0-9]{1,2}(?: [A-Z])?(?: [A-Z]*)? [0-9]{4}\$"
+    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +22,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.edtUdyamAadharNo.paintFlags = View.GONE
-//        card_no = binding.edtUdyamAadharNo.text.toString()
         binding.edtUdyamAadharNo.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
@@ -30,23 +29,32 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 card_no = binding.edtUdyamAadharNo.text.toString()
-//                if (s?.get(7)!!.equals("-") || s?.get(10)!!.equals("-") ){
-//                    Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
-//                }
             }
 
             override fun afterTextChanged(s: Editable?) {
+                val inputlength: Int = binding.edtUdyamAadharNo.text.toString().length
+
                 if (!s.toString().startsWith("UDYAM-")) {
                     s!!.insert(0, "UDYAM-")
-//                    s!!.insert(7,"-")
-//                    s!!.insert(10,"-")
-
                 }
-//                else if (binding.edtUdyamAadharNo.text.insert() === 8||binding.edtUdyamAadharNo.text.get(8)||card_no.indices === 8|| card_no.get(8)){
-//                    if (!s.toString().endsWith("-")) {
-//                        s?.append("-");
-//                    }
-//                }
+                else if(count <= inputlength && inputlength == 8 || inputlength == 11){
+         binding.edtUdyamAadharNo.setText(binding.edtUdyamAadharNo.text.toString() + "-")
+                    val pos: Int = binding.edtUdyamAadharNo.text.length
+                    binding.edtUdyamAadharNo.setSelection(pos)
+                }
+                else if (count >= inputlength && (inputlength == 8 || inputlength == 11)){
+                    binding.edtUdyamAadharNo.setText(binding.edtUdyamAadharNo.text.toString().substring(0, binding.edtUdyamAadharNo.text
+                        .toString().length - 1))
+                    val pos: Int = binding.edtUdyamAadharNo.text.length
+                    binding.edtUdyamAadharNo.setSelection(pos)
+                }
+                else if (binding.edtUdyamAadharNo.text!!.matches(cardPattern.toRegex())){
+                    Toast.makeText(applicationContext, "Valid Card Number", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
+                }
+                count = binding.edtUdyamAadharNo.text.toString().length
             }
         })
 
@@ -54,11 +62,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun validateCard(view: View) {
-        binding.tvUdyam.text = card_no
         if (card_no!!.matches(cardPattern.toRegex())) {
             Toast.makeText(applicationContext, "Valid Card Number", Toast.LENGTH_SHORT).show()
-        }else if(!card_no?.elementAt(8)!!.equals("-") ||!card_no?.get(11)!!.equals("-") ) {
-            Toast.makeText(applicationContext, "Separators already added", Toast.LENGTH_SHORT).show()
         }
         else {
             Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
@@ -66,124 +71,3 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
-
-//val  CARD_NUMBER_TOTAL_SYMBOLS = 13; // size of pattern 0000-0000-0000-0000
-//    val CARD_NUMBER_TOTAL_DIGITS = 11; // max numbers of digits in pattern: 0000 x 4
-//    val CARD_NUMBER_DIVIDER_MODULO = 2; // means divider position is every 5th symbol beginning with 1
-//    val CARD_NUMBER_DIVIDER_POSITION = CARD_NUMBER_DIVIDER_MODULO - 1; // means divider position is every 4th symbol beginning with 0
-//    val CARD_NUMBER_DIVIDER = '-'
-//binding.edtCardNo.addTextChangedListener(object : TextWatcher{
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//            }
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-////                Selection.setSelection(binding.edtCardNo.text, binding.edtCardNo.text!!.length);
-//
-////                            if (!isInputCorrect(s as Editable, CARD_NUMBER_TOTAL_SYMBOLS, CARD_NUMBER_DIVIDER_MODULO, CARD_NUMBER_DIVIDER)) {
-////                    s!!.replace(0, s!!.length, concatString(getDigitArray(s as Editable, CARD_NUMBER_TOTAL_DIGITS)!!, CARD_NUMBER_DIVIDER_POSITION, CARD_NUMBER_DIVIDER));
-////                }
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {
-//                if (!isInputCorrect(s!!, CARD_NUMBER_TOTAL_SYMBOLS, CARD_NUMBER_DIVIDER_MODULO, CARD_NUMBER_DIVIDER)) {
-//                    s!!.replace(0, s!!.length, concatString(getDigitArray(s, CARD_NUMBER_TOTAL_DIGITS)!!, CARD_NUMBER_DIVIDER_POSITION, CARD_NUMBER_DIVIDER));
-//                }
-//            }
-//
-//        })
-//    private fun isInputCorrect(
-//        s: Editable,
-//        size: Int,
-//        dividerPosition: Int,
-//        divider: Char
-//    ): Boolean {
-//        var isCorrect = s.length <= size
-//        for (i in 0 until s.length) {
-//            isCorrect = if (i > 0 && (i + 1) % dividerPosition == 0) {
-//                isCorrect and (divider == s[i])
-//            } else {
-//                isCorrect and Character.isDigit(s[i])
-//            }
-//        }
-//        return isCorrect
-//    }
-//
-//    private fun concatString(digits: CharArray, dividerPosition: Int, divider: Char): String? {
-//        val formatted = StringBuilder()
-//        for (i in digits.indices) {
-//            if (digits[i].toInt() != 0) {
-//                formatted.append(digits[i])
-//                if (i > 0 && i < digits.size - 1 && (i + 1) % dividerPosition == 0) {
-//                    formatted.append(divider)
-//                }
-//            }
-//        }
-//        return formatted.toString()
-//    }
-//
-//    private fun getDigitArray(s: Editable, size: Int): CharArray? {
-//        val digits = CharArray(size)
-//        var index = 0
-//        var i = 0
-//        while (i < s.length && index < size) {
-//            val current = s[i]
-//            if (Character.isDigit(current)) {
-//                digits[index] = current
-//                index++
-//            }
-//            i++
-//        }
-//        return digits
-//    }
-
-
-//binding.edtCardNo.setOnClickListener {
-//    binding.edtCardNo.addTextChangedListener(object : TextWatcher{
-//        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-//
-//        }
-//
-//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//
-//        }
-//
-//        override fun afterTextChanged(s: Editable?) {
-//
-//            if(!s.toString().startsWith("+91")) {
-//                binding.edtCardNo.setText("+91")
-//                Selection.setSelection(binding.edtCardNo.text, binding.edtCardNo.text!!.length)
-//            }
-//        }
-//
-//    })
-//}
-
-
-
-//binding.edtCardNo.setOnClickListener{
-//    binding.edtCardNo.setText("+91")
-//    binding.edtCardNo.addTextChangedListener(object: TextWatcher {
-//        override fun beforeTextChanged(
-//            s: CharSequence?,
-//            start: Int,
-//            count: Int,
-//            after: Int
-//        ) {
-//
-//        }
-//
-//        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//
-//        }
-//
-//        override fun afterTextChanged(s: Editable?) {
-//            if(!s.toString().startsWith("+91")){
-//                binding.edtCardNo.setText("+91");
-//                Selection.setSelection(binding.edtCardNo.text, binding.edtCardNo.text!!.length);
-//
-//            }
-//        }
-//
-//    })
-//}
