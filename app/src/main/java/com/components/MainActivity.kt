0db37c5655d3e98866168b00920e3a8a70583ee2
Kavita Manocha <kavita28978@gmail.com>
2022-07-13 16:07:52 +0530

    Able to delete prespecified hiphen(-) added to text

--- app/src/main/java/com/components/MainActivity.kt
+++ app/src/main/java/com/components/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private var card_no: String? = null
     private val cardPattern = "^[A-Z]{2}[ -][0-9]{1,2}(?: [A-Z])?(?: [A-Z]*)? [0-9]{4}\$"
     var count = 0
+    val space: Char = '-'
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,46 +29,54 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                card_no = binding.edtUdyamAadharNo.text.toString()
+                if ((binding.edtUdyamAadharNo.text.toString().length == 8 || binding.edtUdyamAadharNo.text.toString().length == 11)) {
+                    if (before - count < 0) {
+                        binding.edtUdyamAadharNo.setText(binding.edtUdyamAadharNo.text.toString() + "-");
+                        binding.edtUdyamAadharNo.setSelection(binding.edtUdyamAadharNo.text.toString().length);
+                    }
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {
-                val inputlength: Int = binding.edtUdyamAadharNo.text.toString().length
-
                 if (!s.toString().startsWith("UDYAM-")) {
                     s!!.insert(0, "UDYAM-")
-                }
-                else if(count <= inputlength && inputlength == 8 || inputlength == 11){
-         binding.edtUdyamAadharNo.setText(binding.edtUdyamAadharNo.text.toString() + "-")
-                    val pos: Int = binding.edtUdyamAadharNo.text.length
-                    binding.edtUdyamAadharNo.setSelection(pos)
-                }
-                else if (count >= inputlength && (inputlength == 8 || inputlength == 11)){
-                    binding.edtUdyamAadharNo.setText(binding.edtUdyamAadharNo.text.toString().substring(0, binding.edtUdyamAadharNo.text
-                        .toString().length - 1))
-                    val pos: Int = binding.edtUdyamAadharNo.text.length
-                    binding.edtUdyamAadharNo.setSelection(pos)
+
                 }
                 else if (binding.edtUdyamAadharNo.text!!.matches(cardPattern.toRegex())){
                     Toast.makeText(applicationContext, "Valid Card Number", Toast.LENGTH_SHORT).show()
                 }
-                else{
-                    Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
+                else {
+                    Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT)
+                        .show()
+                    binding.edtUdyamAadharNo.removeTextChangedListener(this)
+                    s?.replace(0, s.length, s.toString());
+                    binding.edtUdyamAadharNo.addTextChangedListener(this)
                 }
                 count = binding.edtUdyamAadharNo.text.toString().length
+//               binding.edtUdyamAadharNo.removeTextChangedListener(this)
+
             }
         })
-
 
     }
 
     fun validateCard(view: View) {
-        if (card_no!!.matches(cardPattern.toRegex())) {
-            Toast.makeText(applicationContext, "Valid Card Number", Toast.LENGTH_SHORT).show()
+        val inputlength = binding.edtUdyamAadharNo.text.toString().length
+        if (inputlength > 0) {
+            binding.edtUdyamAadharNo.text.delete(inputlength - 1, inputlength).trim()
+//            binding.edtUdyamAadharNo.text.trim()
         }
-        else {
-            Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
-        }
+//        binding.edtUdyamAadharNo.text.delete(6,8)
+//        if (card_no!!.matches(cardPattern.toRegex())) {
+//            Toast.makeText(applicationContext, "Valid Card Number", Toast.LENGTH_SHORT).show()
+//        }
+//        else {
+//            Toast.makeText(applicationContext, "Invalid Card Number", Toast.LENGTH_SHORT).show()
+//        }
+
+//        binding.edtUdyamAadharNo.removeTextChangedListener(this)
+//                s?.replace(0, s.length, s.toString());
+//                binding.edtUdyamAadharNo.addTextChangedListener(this)
     }
 
 }
